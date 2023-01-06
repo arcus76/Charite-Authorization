a35564f0c10585d65467d6dc7ef899c10a3cabaa
Artem Kovalevski <quanaris@mi.fu-berlin.de>
2023-01-06 01:33:21 +0100

    Initial commit of Authentification server Code, based on authentification server sample by jgrandja
    https://github.com/spring-projects/spring-authorization-server/tree/main/samples/default-authorizationserver

---
+++ src/main/java/com/fu/berlin/chariteauthorization/ChariteauthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.fu.berlin.chariteauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChariteauthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChariteauthorizationApplication.class, args);
+	}
+
+}
